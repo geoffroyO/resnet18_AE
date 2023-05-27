1a782cf2d99b60208c31fca510ebb61546b7adf4
Geoffroy <geoffroy.oudoumanessah@gmail.com>
2023-05-27 19:36:15 +0200

    profiling code

--- train.py
+++ train.py
@@ -8,6 +8,9 @@ from forward_step import ComputeLoss
 
 import numpy as np
 
+from torch.profiler import profile, record_function, ProfilerActivity
+
+
 from pyJoules.handler.csv_handler import CSVHandler
 from pyJoules.energy_meter import EnergyContext
 
@@ -27,32 +30,39 @@ class TrainerAE:
         self.model.train()
         min_loss = torch.inf
         hist_loss = []
+
         with EnergyContext(handler=self.csv_handler, start_tag='0') as ctx:
-            for epoch in range(self.args.num_epochs):
-                total_loss = 0
-                for x in Bar(self.train_loader):
-                    x = x.float().to(self.device)
-                    optimizer.zero_grad()
+            with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+                for epoch in range(self.args.num_epochs):
+                    total_loss = 0
+                    #for x in Bar(self.train_loader):
+                    train_loader = iter(self.train_loader)
+                    for k in range(5):
+                        with record_function("load_next batch"):
+                            x = next(train_loader)
+                        with record_function("to device batch"):
+                            x = x.float().to(self.device)
+                        with record_function("training_function"):
+                            optimizer.zero_grad()
+                            
+                            x_hat= self.model(x)
+
+                            loss = self.compute.forward(x, x_hat)
+                            loss.backward(retain_graph=True)
+                            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 5)
+                            optimizer.step()
+                            total_loss += loss.item()
+                    if total_loss < min_loss:
+                            min_loss = total_loss
+                            torch.save(self.model.state_dict(), self.args.save_path + 'best-model-parameters.pt')
+                            torch.save(optimizer.state_dict(), self.args.save_path + 'best-optim-parameters.pt')
                     
-                    x_hat= self.model(x)
-
-                    loss = self.compute.forward(x, x_hat)
-                    loss.backward(retain_graph=True)
-                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), 5)
-                    optimizer.step()
-
-                    total_loss += loss.item()
+                    print('Training AE... Epoch: {}, Loss: {:.3f}'.format(
+                        epoch, total_loss/len(self.train_loader)))
+                    hist_loss.append(total_loss/len(self.train_loader))
+                    ctx.record(tag=f'{epoch+1}')
+                    prof.export_chrome_trace(args.save_path + "trace.json")
                     break
-                if total_loss < min_loss:
-                        min_loss = total_loss
-                        torch.save(self.model.state_dict(), self.args.save_path + 'best-model-parameters.pt')
-                        torch.save(optimizer.state_dict(), self.args.save_path + 'best-optim-parameters.pt')
-                
-                print('Training AE... Epoch: {}, Loss: {:.3f}'.format(
-                    epoch, total_loss/len(self.train_loader)))
-                hist_loss.append(total_loss/len(self.train_loader))
-                ctx.record(tag=f'{epoch+1}')
-                break
         self.csv_handler.save_data()
         hist_loss = np.array(hist_loss)
         np.save(self.args.save_path + 'hist_loss.npy', hist_loss)
